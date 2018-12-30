c4818337236c5052f6530d43faa725fffd89a491
Jan Cach <cach.jan@gmail.com>
2018-12-30 23:28:10 +0100

    Create gRPC tutorial

--- client/main.go
+++ client/main.go
@@ -1,5 +1,108 @@
-package client
+package main
+
+import (
+	pb "first_grpc/customer"
+	"context"
+	"log"
+	"io"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func CreateCustomer(client pb.CustomerClient, customer *pb.CustomerRequest){
+	resp, err := client.CreateCustomer(context.Background(),customer)
+	if err != nil {
+		log.Fatalf("Could not create Customer: %v \n", err)
+	}
+
+	if resp.Sucess {
+		log.Printf("A new customer has been added with id: %d \n", resp.Id)
+	}
+}
+
+func GetCustomers(client pb.CustomerClient, filter  *pb.CustomerFilter){
+
+	//calling streaming API
+	stream, err := client.GetCustomers(context.Background(),filter)
+	if err != nil {
+		log.Fatalf("Error on get customers: %v\n", err)
+	}
+
+	for {
+		customer, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("%v.GetCustomers(_) = _,%v\n", client, err)
+		}
+
+		log.Printf("Customer: %v\n", customer)
+	}
+}
 
 func main() {
-	
-}
+
+	conn, err := grpc.Dial(address,grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	//Create new customer client
+	client := pb.NewCustomerClient(conn)
+
+	customer1 := &pb.CustomerRequest{
+		Id: 101,
+		Name: "Karel Novak",
+		Email: "novak12@seznam.cz",
+		Phone: "111222333",
+		Addresses: []*pb.CustomerRequest_Address{
+			&pb.CustomerRequest_Address{
+				Street: "Sladkovskeho",
+				City: "Pardubice",
+				State: "Czech Republic",
+				Zip: "53374",
+				IsShippingAddress: true,
+			},
+			&pb.CustomerRequest_Address{
+				Street: "Pod vilami",
+				City: "Praha",
+				State: "Czech Republic",
+				Zip: "50002",
+				IsShippingAddress: false,
+			},
+		},
+	}
+
+	// create new customer
+	CreateCustomer(client, customer1)
+
+	customer2 := &pb.CustomerRequest{
+		Id: 102,
+		Name: "Iren Rose",
+		Email: "i2rose@gmail.com",
+		Phone: "725-432-565",
+		Addresses: []*pb.CustomerRequest_Address{
+			&pb.CustomerRequest_Address{
+				Street: "1 st mission street",
+				City: "San Francisco",
+				State: "CA",
+				Zip: "94105",
+				IsShippingAddress: true,
+			},
+		},
+	}
+
+	CreateCustomer(client,customer2)
+
+	//Filter with an empty keyword
+	filter := &pb.CustomerFilter{Keyword:""}
+	GetCustomers(client,filter)
+
+	}
--- server/main.go
+++ server/main.go
@@ -1,5 +1,58 @@
-package server
+package main
+
+import (
+	pb "first_grpc/customer"
+	"context"
+	"strings"
+	"net"
+	"log"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+//server is used to implement customer.CustomerServer
+type server struct {
+	savedCustomers []*pb.CustomerRequest
+
+}
+
+//CreateCustomer creates a new customer
+func (s *server) CreateCustomer(ctx context.Context, in *pb.CustomerRequest)(*pb.CustomerResponse, error){
+	s.savedCustomers = append(s.savedCustomers, in)
+	return &pb.CustomerResponse{Id: in.Id, Sucess: true}, nil
+}
+
+
+func (s *server) GetCustomers(filter *pb.CustomerFilter, stream pb.Customer_GetCustomersServer) error{
+	for _, customer := range s.savedCustomers{
+		if filter.Keyword != ""{
+			if !strings.Contains(customer.Name, filter.Keyword){
+				continue
+			}
+		}
+		err := stream.Send(customer)
+		if err != nil {
+			return err
+		}
+
+
+		}
+		return nil
+	}
+
 
 func main() {
-	
-}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v ",err)
+	}
+
+	//create new gRPC server
+	s := grpc.NewServer()
+	pb.RegisterCustomerServer(s, &server{})
+	s.Serve(lis)
+	}
